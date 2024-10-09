0d4bd290daa7b6d73f0e7ae392b6a6c3630514d4
Niraj Kumar <niraj.kiul@gmail.com>
2024-10-09 13:25:54 +0530

    Update builder-design-pattern.js

--- builder-design-pattern.js
+++ builder-design-pattern.js
@@ -1,5 +1,5 @@
-Without the Builder Pattern
-1). Using Constructor with Many Parameters
+//Without the Builder Pattern
+//Using Constructor with Many Parameters
 class Car {
   constructor(make, model, year, color, hasGPS, hasSunroof) {
     this.make = make;
@@ -17,4 +17,65 @@ class Car {
 
 // Creating the object
 const car = new Car('Tesla', 'Model S', 2024, 'Red', true, false);
+console.log(car.describe());
+
+//With the Builder Pattern
+//Using the Builder Pattern
+// Car class remains simple, no complex constructor
+class Car {
+  constructor(builder) {
+    this.make = builder.make;
+    this.model = builder.model;
+    this.year = builder.year;
+    this.color = builder.color;
+    this.hasGPS = builder.hasGPS;
+    this.hasSunroof = builder.hasSunroof;
+  }
+
+  describe() {
+    return `${this.year} ${this.make} ${this.model} in ${this.color} color with GPS: ${this.hasGPS} and Sunroof: ${this.hasSunroof}`;
+  }
+}
+
+// CarBuilder class for step-by-step object creation
+
+class CarBuilder {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  setYear(year) {
+    this.year = year;
+    return this;
+  }
+
+  setColor(color) {
+    this.color = color;
+    return this;
+  }
+
+  addGPS() {
+    this.hasGPS = true;
+    return this;
+  }
+
+  addSunroof() {
+    this.hasSunroof = true;
+    return this;
+  }
+
+  build() {
+    return new Car(this); // Builds and returns the Car object
+  }
+}
+
+// Creating the object using the builder
+
+const car = new CarBuilder('Tesla', 'Model S')
+  .setYear(2024)
+  .setColor('Red')
+  .addGPS()
+  .build();
+
 console.log(car.describe());
